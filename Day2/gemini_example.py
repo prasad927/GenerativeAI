240fd49e904c08b445a326243dd60f9321286222
prasad927 <chaudhariprasad69@gmail.com>
2025-04-17 14:24:06 +0530

    example codes
    Signed-off-by: prasad927 <chaudhariprasad69@gmail.com>

---
+++ Day2/gemini_example.py
@@ -0,0 +1,10 @@
+from google import genai
+from google.genai import types
+ 
+client = genai.Client(api_key='API-KEY')
+ 
+response = client.models.generate_content(
+    model='gemini-2.0-flash-001', contents='Why is the sky blue?'
+)
+
+print(response.text)
